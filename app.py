69c5e1905ac14fb519804cf2498bde29d5369105
jaanhay <122115952+jaanhay@users.noreply.github.com>
2023-12-10 18:15:22 +0000

    added css

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-if __name__ =='__main__':
-  app.run('0.0.0.0',debug=True)
+  return render_template('home.html')
+
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', debug=True)
